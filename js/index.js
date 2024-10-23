2c141e07e7e2e36e0cc351d2d31098d865d52b96
dmitry.mazhnik <dmitry-mazhnik@yandex.ru>
2024-10-23 22:31:05 +0300

    feat: add footer section

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//Раскраска логотипа
 const logoPaths = document.querySelectorAll(".header__logo g path");
 const link = document.querySelector(".header__menu-link");
 
@@ -11,4 +12,19 @@ link.addEventListener("mouseout", (e) => {
   logoPaths.forEach((elem) => {
     elem.style.fill = "rgba(27, 31, 33, 1)";
   });
+});
+
+//Анимация header
+const header = document.querySelector(".header");
+let lastScrollTop = 0;
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.scrollY || document.documentElement.scrollTop;
+
+  if (currentScroll > lastScrollTop) {
+    header.style.transform = "translateY(-100%)";
+  } else {
+    header.style.transform = "translateY(0)";
+  }
+  lastScrollTop = currentScroll;
 });
