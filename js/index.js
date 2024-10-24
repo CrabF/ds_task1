90d3e7318b99790a400fc52b0fdb59234b1dd9d2
dmitry.mazhnik <dmitry-mazhnik@yandex.ru>
2024-10-24 20:36:42 +0300

    feat: add validation for subscribe and background for webinars

--- js/index.js
+++ js/index.js
@@ -28,3 +28,45 @@ window.addEventListener("scroll", () => {
   }
   lastScrollTop = currentScroll;
 });
+
+//Валидация формы
+const form = document.querySelector("form");
+const input = form.querySelector("input[type=email]");
+const button = form.querySelector("button");
+const spanError = form.querySelector(".subscribe__span-error");
+const checkbox = form.querySelector("input[type=checkbox]");
+const inputContainer = form.querySelector(".subscribe__input-container");
+const successForm = document.querySelector(".subscribe__success-form");
+
+form.addEventListener("submit", (e) => {
+  successForm.classList.remove("subscribe__success-form_view");
+  e.preventDefault();
+  if (input.classList.contains("subscribe__input_correct")) {
+    form.remove();
+    successForm.classList.add("subscribe__success-form_view");
+  } else {
+    input.classList.add("subscribe__input_error");
+    spanError.classList.add("subscribe__span-error_view");
+    inputContainer.classList.add("subscribe__input-container_view");
+  }
+});
+
+input.addEventListener("input", (e) => {
+  validateEmail(e.target.value);
+});
+
+function validateEmail(val) {
+  input.classList.remove("subscribe__input_error");
+  input.classList.remove("subscribe__input_correct");
+  spanError.classList.remove("subscribe__span-error_view");
+  inputContainer.classList.remove("subscribe__input-container_view");
+  inputContainer.classList.remove("subscribe__input-container_correct");
+  if (!val.includes("@") || !val.includes(".")) {
+    input.classList.add("subscribe__input_error");
+    spanError.classList.add("subscribe__span-error_view");
+    inputContainer.classList.add("subscribe__input-container_view");
+  } else {
+    input.classList.add("subscribe__input_correct");
+    inputContainer.classList.add("subscribe__input-container_correct");
+  }
+}
